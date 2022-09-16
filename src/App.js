9b2cb3bb33d3840152ee830e3f89d3f4e0378e4e
igorkrasilchuk <virgin.igor@gmail.com>
2022-09-16 22:38:34 +0200

    clear project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
+
 import './App.css';
 
+import Simpson from "./components/simpson/Simpson";
 function App() {
   return (
-    <div className="main">
+      <div className='main'>
+          <div className='header'>
+              <h1>Simpson Family</h1>
+          </div>
+          <div className='title'><h3>Parents</h3></div>
+          <div className='parents'>
+              <Simpson
+                  itemName = {'Homer'}
+                  pic = {'https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'}
+              />
+              <Simpson
+                  itemName = {'Marge'}
+                  pic = {'https://upload.wikimedia.org/wikipedia/en/0/0b/Marge_Simpson.png'}
+              />
+          </div>
+        <div className='title'><h3>Children</h3></div>
+          <div className='children'>
+              <Simpson
+                  itemName = {'Bart'}
+                  pic = {'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png'}
+              />
+              <Simpson
+                  itemName = {'Lisa'}
+                  pic = {'https://upload.wikimedia.org/wikipedia/en/e/ec/Lisa_Simpson.png'}
+              />
+              <Simpson
+                  itemName = {'Maggie'}
+                  pic = {'https://upload.wikimedia.org/wikipedia/en/9/9d/Maggie_Simpson.png'}
+              />
+          </div>
+          <div className='title'><h3>Grandparents</h3></div>
+          <div className= 'grandparents'>
+              <Simpson
+                  itemName = {'Abraham'}
+                  pic = {'https://upload.wikimedia.org/wikipedia/en/3/3e/Abe_Simpson.png'}
+              />
+              <Simpson
+                  itemName = {'Mona '}
+                  pic = {'https://upload.wikimedia.org/wikipedia/en/thumb/7/7f/Mona_Simpson_%28The_Simpsons%29.png/150px-Mona_Simpson_%28The_Simpsons%29.png'}
+              />
+          </div>
 
-    </div>
+
+
+      </div>
   );
 }
 
